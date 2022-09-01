27914acc3c1b81392525dc10731f1b5c42e3e47b
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-09-01 09:12:07 -0300

    style

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
-@section('title', 'Editar o usuário {{ $user->name }}')
+@section('title', "Editar o Usuário {$user->name}")
 
 @section('content')
-<h1>Editar o usuário {{ $user->name }}</h1>
+<h1 class="text-2xl font-semibold leading-tigh py-2">Editar o Usuário {{ $user->name }}</h1>
 
 @include('includes.validations-form')
 
-<form action="{{ route('users.update', $user->id) }}" method="post">
-  @method('PUT')
-  @csrf
-  @include('users._partials.form');
+<form action="{{ route('users.update', $user->id) }}" method="post" enctype="multipart/form-data">
+    @method('PUT')
+    @include('users._partials.form')
 </form>
+
 @endsection
