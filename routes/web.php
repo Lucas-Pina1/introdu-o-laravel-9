e260fa5bd1a8935c48d0ae5e572d815eb6dfbb18
Lucas Pina <ti.lucaspina@gmail.com>
2022-09-01 20:29:54 -0300

    aula 26

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store'); 
-Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create'); 
-Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index'); 
+Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+Route::get('/users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
+Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
 
 Route::delete('/users/{id}', [UserController::class, 'delete'])->name('users.delete');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
